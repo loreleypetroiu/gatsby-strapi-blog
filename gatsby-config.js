77297721a89096e01f200007f568716b4abf29b0
loreleypetroiu <petroiu.loreley@gmail.com>
2021-03-26 13:40:51 +0200

    api url

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL,
         contentTypes: ["article", "category", "writer"],
         singleTypes: [`homepage`, `global`],
         queryLimit: 5000,
